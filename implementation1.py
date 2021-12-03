c63198a3ca846c10af47c3dbbe445002259d48be
Joep de Jong <j.dejong-16@student.tudelft.nl>
2021-12-03 08:20:49 +0100

    Move default functions to main file

--- implementation1.py
+++ implementation1.py
@@ -1,29 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from main import *
+
 
 epsilon = 0.1
-def direct_solve(N,h,epsilon):
-    A = A_matrix(N,h,epsilon)
-
-    f = np.zeros(N+1)
-    f[0] = 1
-
-    u = np.linalg.solve(A,f)
-    x = np.linspace(0,1,N+1)
-    return x, u
-
-def A_matrix(N,h,epsilon):
-    A = np.zeros((N+1,N+1))
-    np.fill_diagonal(A, (2*epsilon + h)/h**2)
-    A[0,0] = 1
-    A[-1,-1] = 1
-    A += np.diag(np.full(N,-epsilon/h**2),1)
-    A[0,1] = 0
-
-    A += np.diag(np.full(N,(-epsilon-h)/h**2),-1)
-    A[-1,-2] = 0
-    return A
 
 
 
