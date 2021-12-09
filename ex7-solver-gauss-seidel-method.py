3112b91a1d628ef298e4bdd03054ec24d673be87
Jules <95241984+JulesFleuren@users.noreply.github.com>
2021-12-09 19:00:52 +0100

    Gauss-Seidel toegevoegd

---
+++ ex7-solver-gauss-seidel-method.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+from main import A_matrix, f_N
+
+def single_step_Gauss_seidel(A,f,u):
+    for i in range(len(f))
+        u[i] = u[i] + (f[i] - np.sum(A[i,:]*u))/A[i,i]
+    return u
+
+def Gauss_seidel_iteration_method(A, f, TOL):
+    u = np.zeros(len(f))
+
+    k = 0
+    sr = []
+    res = 2*TOL
+    while res > TOL:
+        u = single_step_Gauss_seidel(A,f,u)
+
+        res = np.linalg.norm(f - np.matmul(A, u))/np.linalg.norm(f)
+        sr.append(res)
+        k += 1
+    return u, sr, k
--- implementation1.py
+++ implementation1.py
@@ -7,31 +7,30 @@ from main import *
 epsilon = 0.1
 
 
+# ex. 1
+N=4
+h=1/N
+epsilon = np.logspace(0,-2,3)
+# epsilon = 10**(-2)
+x_ref = np.linspace(0,1,50)
+X_ref, Epsilon = np.meshgrid(x_ref,epsilon)
+epsilon = 1
+U_ref = (np.exp(X_ref/Epsilon) - np.exp(1/Epsilon))/(1 - np.exp(1/Epsilon))
 
+plt.plot(*direct_solve(N,h,epsilon), label="numerical solution")
+plt.plot(X_ref.T, U_ref.T, label="reference solution")
 
-# # ex. 1
-# N=4
-# h=1/N
-# epsilon = np.logspace(0,-4,8)
-# x_ref = np.linspace(0,1,50)
-# X_ref, Epsilon = np.meshgrid(x_ref,epsilon)
+plt.legend()
+
+# # table of errors
 # epsilon = 0.5
-# U_ref = (np.exp(X_ref/Epsilon) - np.exp(1/Epsilon))/(1 - np.exp(1/Epsilon))
-
-# plt.plot(*direct_solve(N,h,epsilon), label="numerical solution")
-# plt.plot(X_ref.T, U_ref.T, label="reference solution")
-
-# plt.legend()
-
-# table of errors
-epsilon = 0.5
-print("epsilon", "N", "error", sep = '\t')
-for epsilon in [0.5, 0.1, 0.01]:
-    for N in [2**n for n in range(4,12)]:
-        x, u = direct_solve(N,1/N,epsilon)
-        u_ref =  (np.exp(x/epsilon) - np.exp(1/epsilon))/(1 - np.exp(1/epsilon))
-        max_difference = np.max(np.abs(u-u_ref))
-        print(epsilon, N, max_difference, sep="\t")
+# print("epsilon", "N", "error", sep = '\t')
+# for epsilon in [0.5, 0.1, 0.01]:
+#     for N in [2**n for n in range(4,12)]:
+#         x, u = direct_solve(N,1/N,epsilon)
+#         u_ref =  (np.exp(x/epsilon) - np.exp(1/epsilon))/(1 - np.exp(1/epsilon))
+#         max_difference = np.max(np.abs(u-u_ref))
+#         print(epsilon, N, max_difference, sep="\t")
 
 # # Richardson error estimate
 # N = 128
@@ -44,14 +43,14 @@ for epsilon in [0.5, 0.1, 0.01]:
 # p = np.log2((u_2[i//2]-u_4[i//4])/(u_1[i]-u_2[i//2]))
 # print("order p of accuracy of solution:", p)
 
-# eigenvalues, eigenvectors of A
-N=100
-h=1/N
-epsilon = 0.5
-w, v = np.linalg.eig(A_matrix(N,h,epsilon))
-print(w)
+# # eigenvalues, eigenvectors of A
+# N=100
+# h=1/N
+# epsilon = 0.5
+# w, v = np.linalg.eig(A_matrix(N,h,epsilon))
+# print(w)
 
-plt.scatter(np.arange(len(w)), np.sort(w))
+# plt.scatter(np.arange(len(w)), np.sort(w))
 
 # plt.scatter(np.arange(len(w)), v[:,0])
 # plt.scatter(np.arange(len(w)), v[:,1])
