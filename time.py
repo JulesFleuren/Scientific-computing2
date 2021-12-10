59216dceff2dcbf5f425356a299c295257b7c0fe
Joep de Jong <j.dejong-16@student.tudelft.nl>
2021-12-10 19:33:45 +0100

    Update time.py config

--- time.py
+++ time.py
@@ -11,16 +11,16 @@ from main import A_matrix, f_N
 import ex7_solver_gauss_seidel_method_cython as gs_cy
 import ex7_solver_gauss_seidel_method as gs_py
 
-N = 2**4
+N = 2**6
 h = 1/N
 epsilon = 0.1
 
 A = A_matrix(N, h, epsilon)
 f = f_N(N)
-TOL = 1e-10
+TOL = 1e-6
 """
 
-ctime = timeit(stmt="gs_cy.gauss_seidel_iteration_method(A,f,TOL)", setup=setup1, number =100)
-ptime = timeit(stmt="gs_py.gauss_seidel_iteration_method(A,f,TOL)", setup=setup1, number =100)
+ctime = timeit(stmt="gs_cy.gauss_seidel_iteration_method(A,f,TOL)", setup=setup1, number =13)
+ptime = timeit(stmt="gs_py.gauss_seidel_iteration_method(A,f,TOL)", setup=setup1, number =13)
 
 print(ctime,ptime,ptime/ctime)
